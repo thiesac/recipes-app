ca41d76d0361d98a13ab9f5c9244629f4cdf909f
rodolfohro <rodolfohro@gmail.com>
2023-06-06 15:35:42 -0300

    Ajustando caminho do Login

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../helpers/renderWithRouter';
 import App from '../App';
-import Login from '../pages/Login';
+import Login from '../pages/Login/Login';
 
 describe('Login', () => {
   const emailInputTestId = 'email-input';
