639f5c259fd3dd400594a896c098b1f86b52c990
Roberto Mello <robertomelloalmeida@outlook.com>
2023-06-06 17:22:22 -0300

    footer finalizado

--- src/pages/Drinks/Drinks.jsx
+++ src/pages/Drinks/Drinks.jsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 function Drinks() {
   return (
     <div>
       <Header />
       {/* Conteúdo da página */}
+      <Footer />
     </div>
   );
 }
