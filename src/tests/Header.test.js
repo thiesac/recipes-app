9f4aef859ad8b1bca30adcf420a89dca3083b798
rodolfohro <rodolfohro@gmail.com>
2023-06-06 05:38:17 -0300

    melhorado a cobertura de testes do Header ainda mais

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -40,7 +40,7 @@ describe('Header', () => {
     expect(searchIcon).toBeInTheDocument();
   });
 
-  it('deve renderizar o título', () => {
+  it('deve renderizar o título "Meals" na página de refeições', () => {
     render(
       <MemoryRouter initialEntries={ ['/meals'] }>
         <Header />
@@ -51,9 +51,53 @@ describe('Header', () => {
     expect(title).toHaveTextContent('Meals');
   });
 
-  it('deve navegar para a página de perfil ao clicar no ícone de perfil', () => {
+  it('deve renderizar o título "Drinks" na página de bebidas', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/drinks'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    const title = screen.getByTestId('page-title');
+    expect(title).toHaveTextContent('Drinks');
+  });
+
+  it('deve renderizar o título "Profile" na página de perfil', () => {
     render(
       <MemoryRouter initialEntries={ ['/profile'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    const title = screen.getByTestId('page-title');
+    expect(title).toHaveTextContent('Profile');
+  });
+
+  it('deve renderizar o título "Done Recipes" na página de receitas concluídas', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/done-recipes'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    const title = screen.getByTestId('page-title');
+    expect(title).toHaveTextContent('Done Recipes');
+  });
+
+  it('deve renderizar o título "Favorite Recipes" na página de receitas favoritas', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/favorite-recipes'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    const title = screen.getByTestId('page-title');
+    expect(title).toHaveTextContent('Favorite Recipes');
+  });
+
+  it('deve navegar para a página de perfil ao clicar no ícone de perfil', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
         <Header />
       </MemoryRouter>,
     );
@@ -62,5 +106,31 @@ describe('Header', () => {
     userEvent.click(profileIcon);
 
     expect(screen.getByTestId('page-title')).toHaveTextContent('Profile');
+  });
+
+  it('deve navegar para a página de receitas concluídas ao clicar no título "Done Recipes"', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    const doneRecipesTitle = screen.getByTestId('page-title');
+    userEvent.click(doneRecipesTitle);
+
+    expect(screen.getByTestId('page-title')).toHaveTextContent('Done Recipes');
+  });
+
+  it('deve navegar para a página de receitas favoritas ao clicar no título "Favorite Recipes"', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    const favoriteRecipesTitle = screen.getByTestId('page-title');
+    userEvent.click(favoriteRecipesTitle);
+
+    expect(screen.getByTestId('page-title')).toHaveTextContent('Favorite Recipes');
   });
 });
