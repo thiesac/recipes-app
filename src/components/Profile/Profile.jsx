99df9f4f5b7c6920ae52997b0915feb8b02341c5
rodolfohro <rodolfohro@gmail.com>
2023-06-06 21:41:40 -0300

    58-100%

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,6 +7,12 @@ function Profile() {
       <Header />
       <p>Profile</p>
       {/*       Conteúdo da página */}
+      <div>
+        <h1 data-testid="profile-email">example@example.com</h1>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
     </div>
   );
 }
