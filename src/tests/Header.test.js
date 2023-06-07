7960386b6ef60bd501b69558dff533a130e6401e
rodolfohro <rodolfohro@gmail.com>
2023-06-07 20:24:27 -0300

    adicionado testes no Header para 100% de cobertura

--- src/tests/Header.test.js
+++ src/tests/Header.test.js
@@ -1,10 +1,14 @@
+// src/tests/Header.test.js
+
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+/* import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min'; */
 import Header from '../components/Header/Header';
 
 describe('Header', () => {
   const searchButtonTest = 'search-top-btn';
+  const pageTitleTest = 'page-title';
   it('deve exibir o título corretamente com base na rota atual', () => {
     render(
       <MemoryRouter initialEntries={ ['/meals'] }>
@@ -12,7 +16,7 @@ describe('Header', () => {
       </MemoryRouter>,
     );
 
-    expect(screen.getByTestId('page-title')).toHaveTextContent('Meals');
+    expect(screen.getByTestId(pageTitleTest)).toHaveTextContent('Meals');
   });
 
   it('deve exibir o ícone de perfil', () => {
@@ -72,7 +76,27 @@ describe('Header', () => {
     expect(screen.queryByTestId('search-input')).toBeNull();
   });
 
-  /*   it('deve exibir o título "Done Recipes" na página /done-recipes', () => {
+  it('deve definir corretamente o título como "Drinks" quando a página for "/drinks"', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/drinks'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByTestId(pageTitleTest)).toHaveTextContent('Drinks');
+  });
+
+  it('deve definir corretamente o título como "Profile" quando a página for "/profile"', () => {
+    render(
+      <MemoryRouter initialEntries={ ['/profile'] }>
+        <Header />
+      </MemoryRouter>,
+    );
+
+    expect(screen.getByTestId(pageTitleTest)).toHaveTextContent('Profile');
+  });
+
+  it('deve definir corretamente o título como "Done Recipes" quando a página for "/done-recipes"', () => {
     render(
       <MemoryRouter initialEntries={ ['/done-recipes'] }>
         <Header />
@@ -80,9 +104,9 @@ describe('Header', () => {
     );
 
     expect(screen.getByTestId(pageTitleTest)).toHaveTextContent('Done Recipes');
-  }); */
+  });
 
-  /*   it('deve exibir o título "Favorite Recipes" na página /favorite-recipes', () => {
+  it('deve definir corretamente o título como "Favorite Recipes" quando a página for "/favorite-recipes"', () => {
     render(
       <MemoryRouter initialEntries={ ['/favorite-recipes'] }>
         <Header />
@@ -90,5 +114,5 @@ describe('Header', () => {
     );
 
     expect(screen.getByTestId(pageTitleTest)).toHaveTextContent('Favorite Recipes');
-  }); */
+  });
 });
