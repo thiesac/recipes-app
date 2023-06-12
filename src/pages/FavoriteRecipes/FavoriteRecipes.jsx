ebe3249f32680f07426f42c480fda4e5e2e48e6b
Thiesa <thiesa.c@gmail.com>
2023-06-12 15:50:59 -0400

    Req 50-52 sem testar

--- src/pages/FavoriteRecipes/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes/FavoriteRecipes.jsx
@@ -4,13 +4,46 @@ import React from 'react';
 import Header from '../../components/Header/Header';
 
 function FavoriteRecipes() {
+  const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')); // vem do requisito 34
+
   return (
     <div>
       <Header />
-      {/* Conteúdo da página */}
+      {
+        favoriteRecipes.map(
+          (
+            { id, type, nationality, category, alcoholicOrNot, name, image },
+          ) => (
+            <section key={ id }>
+              <img src={ image } alt="name" />
+              <p>{ name }</p>
+              { (
+                type === 'meal'
+                && <span>{`${category} ${nationality}` }</span>
+              ) }
+              { (
+                type === 'drink'
+                && <p>{ alcoholicOrNot }</p>
+              ) }
+              <button>Compartilhar</button>
+              <button>Desfavoritar</button>
+            </section>
+          ),
+        )
+      }
       <p>Favorite Recipes</p>
     </div>
   );
 }
 
 export default FavoriteRecipes;
+
+// [{
+//   id: id - da - receita,
+//   type: meal - ou - drink,
+//   nationality: nacionalidade - da - receita - ou - texto - vazio,
+//   category: categoria - da - receita - ou - texto - vazio,
+//   alcoholicOrNot: alcoholic - ou - non - alcoholic - ou - texto - vazio,
+//   name: nome - da - receita,
+//   image: imagem - da - receita
+// }]
