a94458c633aa0074e46ddec7d8cfdd22a76437e2
Thiesa <thiesa.c@gmail.com>
2023-06-13 15:53:44 -0400

    Função para remover favorito

--- src/pages/FavoriteRecipes/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes/FavoriteRecipes.jsx
@@ -6,90 +6,26 @@ import Header from '../../components/Header/Header';
 import shareIcon from '../../images/shareIcon.svg';
 
 function FavoriteRecipes() {
-  const [currFilterFav, setFilterFav] = useState('');
+  // const [currFilterFav, setFilterFav] = useState('');
   const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes')); // vem do requisito 34
 
-  const copyToClipboard = async (text) => {
-    try {
-      await navigator.clipboard.writeText(text);
-      Swal.fire('Link copied!');
-    } catch (error) {
-      console.error('Failed to copy text:', error);
-    }
-  };
+  // const copyToClipboard = async (text) => {
+  //   try {
+  //     await navigator.clipboard.writeText(text);
+  //     Swal.fire('Link copied!');
+  //   } catch (error) {
+  //     console.error('Failed to copy text:', error);
+  //   }
+  // };
 
-  const drinkCard = (drink, index) => {
-    const { id, type, alcoholicOrNot, name, image } = drink;
-
-    const detailsLink = `${initialURL}/drinks/${idDrink}`;
-    return (
-      <div>
-        <Link to={ `/drinks/${idDrink}` }>
-          <img
-            src={ image }
-            alt={ name }
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
-        </Link>
-        <p data-testid={ `${index}-horizontal-top-text` }>{ alcoholicOrNot }</p>
-        <button
-          data-testid={ `${index}-horizontal-share-btn` }
-          onClick={ () => copyToClipboard(detailsLink) }
-        >
-          <img src={ shareIcon } alt="Share button" />
-        </button>
-      </div>
-    );
-  };
-
-  const foodCard = (food, index) => {
-    const { id, type, nationality, category, name, image } = food;
-
-    const detailsLink = `${initialURL}/drinks/${idMeal}`;
-    return (
-      <div>
-        <Link to={ `/drinks/${idMeal}` }>
-          <img
-            src={ image }
-            alt={ name }
-            data-testid={ `${index}-horizontal-image` }
-          />
-          <p data-testid={ `${index}-horizontal-name` }>{ name }</p>
-        </Link>
-        <p data-testid={ `${index}-horizontal-top-text` }>
-          { `${nationality} - ${category}` }
-        </p>
-        <button
-          data-testid={ `${index}-horizontal-share-btn` }
-          onClick={ () => copyToClipboard(detailsLink) }
-        >
-          <img src={ shareIcon } alt="Share button" />
-        </button>
-      </div>
-    );
-  };
-
-  const showRecipes = useCallback(() => {
-    if (!currFilterFav) {
-      return favoriteRecipes.map((recipe, i) => (
-        recipe.idDrink ? drinkCard(recipe, i) : foodCard(recipe, i)
-      ));
-    } if (currFilterFav === 'meals') {
-      return favoriteRecipes.map((recipe, i) => foodCard(recipe, i));
-    } if (currFilterFav === 'drinks') {
-      return favoriteRecipes.map((recipe, i) => drinkCard(recipe, i));
-    }
-  }, []);
-
-  useEffect(() => showRecipes(), [showRecipes]);
+  
 
   return (
     <div>
       <Header />
       <button
         data-testid="filter-by-all-btn"
-        onClick={ (e) => setFilterFav(e.target.value) }
+        onClick={ (e) => setFilterFav(console.log(e.target.value)) }
         value="all"
       >
         All
