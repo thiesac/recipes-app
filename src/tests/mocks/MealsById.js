56c803e44aad6202d2e7492bc70d2794adaa0b40
Rodolfo Oliveira <rodolfohro@gmail.com>
2023-06-15 16:45:12 -0300

    EsLint

--- src/tests/mocks/MealsById.js
+++ src/tests/mocks/MealsById.js
@@ -1,3 +1,4 @@
+const MockTeaspoon = '1/2 teaspoon';
 const MealsById = {
   meals: [
     {
@@ -6,7 +7,8 @@ const MealsById = {
       strDrinkAlternate: null,
       strCategory: 'Vegetarian',
       strArea: 'Italian',
-      strInstructions: '1. Bring a large pot of water to a boil.\r\n2. Add kosher salt to the boiling water, then add the pasta. Cook the pasta according to the package instructions, about 9 minutes.\r\n3. In a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, about 1 minute.\r\n4. Add the chopped tomatoes, red pepper flakes, salt, and pepper. Reduce the heat to medium low and simmer until the sauce thickens slightly, about 10 minutes.\r\n5. Drain the pasta and add it to the skillet with the sauce. Stir to coat the pasta with the sauce.\r\n6. Serve the pasta in bowls and sprinkle with grated Parmesan cheese, if desired.',
+      strInstructions:
+        '1. Bring a large pot of water to a boil.\r\n2. Add kosher salt to the boiling water, then add the pasta. Cook the pasta according to the package instructions, about 9 minutes.\r\n3. In a large skillet over medium-high heat, add the olive oil and heat until the oil starts to shimmer. Add the garlic and cook, stirring, until fragrant, about 1 minute.\r\n4. Add the chopped tomatoes, red pepper flakes, salt, and pepper. Reduce the heat to medium low and simmer until the sauce thickens slightly, about 10 minutes.\r\n5. Drain the pasta and add it to the skillet with the sauce. Stir to coat the pasta with the sauce.\r\n6. Serve the pasta in bowls and sprinkle with grated Parmesan cheese, if desired.',
       strMealThumb: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
       strTags: 'Pasta,Curry',
       strYoutube: 'https://www.youtube.com/watch?v=1IszT_guI08',
@@ -23,9 +25,9 @@ const MealsById = {
       strMeasure3: '3 cloves',
       strMeasure4: '1 can',
       // eslint-disable-next-line sonarjs/no-duplicate-string
-      strMeasure5: '1/2 teaspoon',
-      strMeasure6: '1/2 teaspoon',
-      strMeasure7: '1/2 teaspoon',
+      strMeasure5: MockTeaspoon,
+      strMeasure6: MockTeaspoon,
+      strMeasure7: MockTeaspoon,
       strMeasure8: 'spinkling',
     },
   ],
