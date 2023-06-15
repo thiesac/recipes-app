c24140df5b8807f57f34890e86ab01949608fdaf
DelioAraujo <delioaraujo@gmail.com>
2023-06-15 13:36:58 -0300

    vaaaai

--- src/pages/RecipeInProgress/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress/RecipeInProgress.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
-// import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 import MealInProgress from './MealInProgress';
 import DrinkInProgress from './DrinkInProgress';
 
