94a66388c0c486672823bcb3070b2ebe241906d2
DelioAraujo <delioaraujo@gmail.com>
2023-06-15 13:26:44 -0300

    ultima alteração

--- src/App.js
+++ src/App.js
@@ -31,12 +31,12 @@ function App() {
           <Route exact path="/drinks/:id-da-receita" component="" />
           <Route
             exact
-            path="/(meals|drinks)/52771/in-progress"
+            path="/(meals|drinks)/:id-da-receita/in-progress"
             component={ RecipeInProgress }
           />
           <Route
             exact
-            path="/(meals|drinks)/178319/in-progress"
+            path="/(meals|drinks)/:id-da-receita/in-progress"
             component={ RecipeInProgress }
           />
           <Route exact path="/profile" component={ Profile } />
